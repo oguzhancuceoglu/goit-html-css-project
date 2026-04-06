4dfe4e1d835ff0ab2fb24364779b3b2e1529399a
ulaskarakas <ulaskarakas95@gmail.com>
2026-04-06 14:28:39 +0300

    Make final adjustment

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
-/* Header - script for mobile menu opening and closing */
-const navBtn = document.querySelector('.nav-btn');
-const closeBtn = document.querySelector('.mobile-menu-close');
+const openBtn = document.querySelector('.nav-btn');
+const closeBtn = document.querySelector('.menu-close-btn');
 const menu = document.querySelector('#mobile-menu');
+const overlay = document.querySelector('.overlay');
+const links = document.querySelectorAll('.mobile-menu a');
 
-navBtn.addEventListener('click', () => {
-  menu.classList.add('is-open');
+// open mobile menu
+openBtn.addEventListener('click', () => {
+  menu.classList.add('show');
+  overlay.classList.add('closing');
+  document.body.style.overflow = 'hidden';
 });
 
-closeBtn.addEventListener('click', () => {
-  menu.classList.remove('is-open');
+// close mobile menu
+const closeMenu = () => {
+  menu.classList.add('closing');
+  menu.classList.remove('show');
+  menu.addEventListener('transitionend', function handler() {
+    menu.classList.remove('closing'); // animasyon bitince kapalı sınıf kaldır
+    menu.removeEventListener('transitionend', handler);
+  });
+  overlay.classList.remove('show');
+  document.body.style.overflow = '';
+};
+
+closeBtn.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+
+links.forEach(link => {
+  link.addEventListener('click', closeMenu);
 });
